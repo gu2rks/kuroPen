dd604f70c1557241b196cd6e02d67d6b7a6e4c54
amata a <ch15mt.ag@gmail.com>
2019-12-14 16:00:19 +0100

    fixed print()

--- findAP.py
+++ findAP.py
@@ -3,13 +3,15 @@ import sys, os, time, random, threading
 from scapy.all import*
 
 devices = set()
-
+ch = 0
 # change ch of interface each 1 sec
 def channel_hopper(interface):
     while True:
         ch = random.randint(1, 13) # pick random ch 1-13
         os.system('iw dev %s set channel %d' % (interface, ch))
+        print('------------------------------------')
         print('jump to channel: %d' %ch)
+        print('------------------------------------')
         time.sleep(2)
 
 def PacketHandler(pkt) :
@@ -17,14 +19,14 @@ def PacketHandler(pkt) :
     if pkt.haslayer(Dot11Beacon) or pkt.haslayer(Dot11ProbeResp):
         if pkt.addr2 not in devices:
             if(str(pkt[Dot11Elt].info) == "b\'\'"):
-                print("HIDDEN : "+pkt.addr3)
+                print('SSID: %s  BSSID: %s ' % ('HIDDEN', pkt.addr3))
             else:
-                print(str(pkt[Dot11Elt].info) +" : "+pkt.addr3)
+                bssid = str(pkt[Dot11Elt].info)
+                print('SSID: %s  BSSID: %s ' % (bssid[2:(len(bssid) - 1)], pkt.addr3))
             devices.add(pkt.addr3)
 
 
 if __name__ == '__main__':
-
     #thread for ch hop
     hopper = threading.Thread(target=channel_hopper, args=(sys.argv[1],))
     hopper.daemon = True
